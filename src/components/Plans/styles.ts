3ece9782b3577ee0405b7f4db7323b60f616d88a
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-01-05 11:54:35 -0300

    refactor: plans responsivity

--- src/components/Plans/styles.ts
+++ src/components/Plans/styles.ts
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 
 export const CardArea = styled.div`
   width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
+  display: flex;
   justify-content: center;
-  align-content: center;
+  grid-gap: 10px;
   margin: ${({ theme }) => theme.spacing * 3}px 0;
-  @media (max-width: 750px) {
+  @media (max-width: 800px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 10px;
